133eb8af8d4396103c188db96c8c547d998e07f2
jdr4343 <jdr4343@naver.com>
2021-07-26 05:43:49 +0900

    Acess Control

--- DevelopingAppsForIosCS193P.1/Memo.swift
+++ DevelopingAppsForIosCS193P.1/Memo.swift
@@ -25,3 +25,26 @@ import Foundation
  
  
  
+ 
+ Access Control
+ 
+ Protexting our internal implementations
+ Likely most of you have only worked on relatively small projects
+ Inside those projects, any object can pretty much call any function in any other object
+ When projects start to get large, though, this vecomes very dicey
+ you want to be able to protect the INTERNAL inplementation of data structures
+ you do this by marking which API* you want other code to yse with certain keywords
+ 
+ 
+ *I.e. methods and properties
+ 
+ protecting our internal implmentations
+ Swift supports this with the following access control keywords ...
+ 1) internal - this is the default, it means "usable by any object in my app or framework"
+ 2) private - this means "only callable from within this object"
+ 3) private(Set) - this means "this property is readable outside this object but not settable"
+ 4) fileprivate - accessible by any code in this source file
+ 5) public - (for frameworks only) this can be uesd by objects outside my frame work
+ 6) open - (for frameworks only) public and objects outside my framework can subclass this
+ 
+ 
