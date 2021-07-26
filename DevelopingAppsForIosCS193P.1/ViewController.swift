b49ce4f4e103a0e141e2de03c679cec6b7192bc4
jdr4343 <jdr4343@naver.com>
2021-07-27 04:15:21 +0900

    ㅊㅊ

--- DevelopingAppsForIosCS193P.1/ViewController.swift
+++ DevelopingAppsForIosCS193P.1/ViewController.swift
@@ -79,27 +79,27 @@ class ViewController: UIViewController {
     
     var emoji: [Int:String] = [:]
     
-   private func emoji(for card: Card) -> String {
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-                emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
-            }
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        }
         return emoji[card.identifier]! ?? "?"
     }
 }
 
-    extension Int {
-        var arc4random: Int {
-    if self > 0 {
-        return Int(arc4random_uniform(UInt32(self)))
-    } else if self < 0 {
-        return -Int(arc4random_uniform(UInt32(abs(self))))
-    }else {
-        return 0
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        }else {
+            return 0
         }
     }
 }
 
-    
+
 
 
 
